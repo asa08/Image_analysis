737fe6110dd558898eef123fbee9ab03b009eb07
maiko-m <mn.6182.nm@gmail.com>
2021-03-04 23:21:02 +0900

    :+1: navigationを追加

--- image_analysis/Screen/HomeViewController/View/HomeViewController.swift
+++ image_analysis/Screen/HomeViewController/View/HomeViewController.swift
@@ -11,6 +11,20 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setNavigation()
+    }
+    
+    func setNavigation() {
+        let rightButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(cameraButtonAction))
+        navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    @objc private func cameraButtonAction(_ sender: UIBarButtonItem) {
+        
     }
 }
 
---
+++ image_analysis/Screen/HomeViewController/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  image_analysis
+//
+//  Created by maiko morisaki on 2021/03/02.
+//
+
+import Foundation
